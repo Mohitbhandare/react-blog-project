47103886b1d6ca3478aa996bc9b75f5097ee6b0e
Mohit Bhandare <mohitbhandare2001@gmail.com>
2023-12-28 13:47:58 +0530

    redux Toolkit setup in project

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import config from './config/config';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+import authServices from './appwrite/auth';
+import { Header, Footer } from './components/index';
 
 function App() {
-  return (
-    <>
-      <h1>React blog {config.appwriteBucketId}</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServices
+      .getCurrentUser()
+      .then((userdata) => {
+        if (userdata) {
+          dispatch(login(userdata));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>TODO:{/* <OutLet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
